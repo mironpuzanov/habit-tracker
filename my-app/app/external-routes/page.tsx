39be045aa2c34c9c532d2dad7a9a4984d9c566bd
Miron Puzanov <your.email@example.com>
2025-03-18 13:21:40 +0100

    Fix: Rename (external) directory to external for better compatibility

--- my-app/app/external-routes/page.tsx
+++
@@ -1,9 +0,0 @@
-import { LandingHero } from "@/components/external";
-
-export default function Home() {
-  return (
-    <div className="flex-1 w-full flex flex-col gap-20 items-center">
-      <LandingHero />
-    </div>
-  );
-} 
